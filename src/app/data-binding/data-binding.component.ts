3c571c7c090b6cdae57bc0823a74bf57b16ba174
Carlos.Inhani <carlos.inhani@gmail.com>
2020-12-10 17:15:12 -0300

    life-cycle

--- src/app/data-binding/data-binding.component.ts
+++ src/app/data-binding/data-binding.component.ts
@@ -49,9 +49,9 @@ export class DataBindingComponent implements OnInit {
     this.isMouseOver = !this.isMouseOver;
   }
 
-  onMudouValor(evento){
-    console.log(evento.novoValor);
-  }
+  // onMudouValor(evento){
+  //   console.log(evento.novoValor);
+  // }
 
   constructor() { }
 
